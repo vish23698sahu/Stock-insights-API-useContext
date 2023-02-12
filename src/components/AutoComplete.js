395970786ce1f9c0a21de617f3953a0c1fffc4f2
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-02-12 22:11:47 +0530

    v - 3 search bar Auto Complete component

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import finnHub from '../apis/finnHub';
 
 export const AutoComplete = () => {
+    const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
+
+    const renderDropdown = () => {
+        const dropdownClass = search ? "show" : null
+        return (
+            <ul
+                style={{
+                    height: "450px",
+                    overflowY: "scroll",
+                    overflowX: "hidden",
+                    cursor: "pointer"
+                }}
+                className={`dropdown-menu ${dropdownClass}`}>
+                {
+                    results.map((result) => {
+                        return (
+                            <li key={result.symbol} className='dropdown-item' >{result.description} ({result.symbol})</li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+
+    useEffect(() => {
+        let isMounted = true
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+                console.log(response)
+
+                if (isMounted)
+                    setResults(response.data.result)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        if (search.length > 0)
+            fetchData();
+        else
+            setResults([]);
+
+        return () => (isMounted = false)
+    }, [search]);
+
+
     return (
-        <div>AutoComplete</div>
+        <div className='w-50 p-5 rounded mx-auto'>
+            <div className='form-floating dropdown'>
+                <input
+                    style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+                    id="search"
+                    type="text"
+                    className="form-control"
+                    placeholder="Search"
+                    autoComplete="off"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                ></input>
+                {/* <label htmlFor="search" >Search</label> */}
+                {renderDropdown()}
+            </div>
+
+        </div>
     )
 }
